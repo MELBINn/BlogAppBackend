e6ea3b8da3dfba432a6a8a5a7dc5895438795242
MELBINn <melbinsunny29@gmail.com>
2024-01-16 03:21:14 +0530

    orgin added

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:"*",credentials:true}))
+app.use(cors({origin:'graceful-gnome-e6fed6.netlify.app',credentials:true}))
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
