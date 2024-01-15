c608a9a04424b21666ec52b61b8a9d894788b893
MELBINn <melbinsunny29@gmail.com>
2024-01-16 03:16:14 +0530

    orgin added

--- routes/auth.js
+++ routes/auth.js
@@ -38,7 +38,7 @@ router.post("/login",async (req,res)=>{
         }
         const token=jwt.sign({_id:user._id,username:user.username,email:user.email},process.env.SECRET,{expiresIn:"3d"})
         const {password,...info}=user._doc
-        res.cookie("token",token).status(200).json(info)
+        res.cookie("token",token,{ domain: 'yourdomain.com', sameSite: 'None' }).status(200).json(info)
 
     }
     catch(err){
