9898561748ec8b4dc15049b96d0b8289f35c3180
MELBINn <melbinsunny29@gmail.com>
2024-01-16 09:25:45 +0530

    orgin added

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(cors({origin:'graceful-gnome-e6fed6.netlify.app',credentials:true}))
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:'graceful-gnome-e6fed6.netlify.app',credentials:true}))
+
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
