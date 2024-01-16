f1790948ce7dfabc359a85915751a970cb140b37
MELBINn <melbinsunny29@gmail.com>
2024-01-16 09:37:52 +0530

    orgin added

--- index.js
+++ index.js
@@ -23,7 +23,12 @@ const connectDB=async()=>{
     }
 }
 
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://graceful-gnome-e6fed6.netlify.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 //middlewares
 app.use(cors({origin:'graceful-gnome-e6fed6.netlify.app',credentials:true}))
